c6eec8f8e79e04a78b188d7f2b4792c4adcc5414
albanovs <albvnovs@gmail.com>
2024-12-01 12:59:37 +0600

    mongoose change

--- models/simcard/managers.mjs
+++ models/simcard/managers.mjs
@@ -1,8 +1,71 @@
 import mongoose from "mongoose";
-import { SimcardSchema } from '../components/Simcard.mjs'
 
 const DataSchema = new mongoose.Schema({
-    SimcardSchema
+    data_register: {
+        type: String,
+        default: "",
+    },
+    curator: {
+        type: String,
+        default: "",
+    },
+    slot: [
+        {
+            num: {
+                type: Number,
+                
+                default: 0,
+            },
+            number: {
+                type: String,
+                default: "",
+            },
+            status: {
+                type: String,
+                default: "",
+            },
+            buyer: {
+                type: String,
+                default: "",
+            },
+            personal_number: {
+                type: String,
+                default: "",
+            },
+            date_of_verification: {
+                type: String,
+                default: "",
+            },
+            days_since_verification: {
+                type: String,
+                default: "",
+            },
+            status_simCard: {
+                type: String,
+                default: "",
+            },
+            physical_simCard: {
+                type: String,
+                default: "",
+            },
+            registration: {
+                type: String,
+                default: "",
+            },
+            WAcod: {
+                type: String,
+                default: "",
+            },
+            TGcod: {
+                type: String,
+                default: "",
+            },
+            data_register: {
+                type: String,
+                default: "",
+            },
+        },
+    ],
 });
 
 const SimcardLeaderManager = mongoose.model('simcardleadermanager', DataSchema);
